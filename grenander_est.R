8ce4ccd48d04cf601b25d4d60dbdd1ea46af392f
ntyet <tienyettoan@gmail.com>
2014-07-08 14:03:23 -0500

    Grenander estimate

--- grenander_est.R
+++ grenander_est.R
@@ -1,30 +1,168 @@
 library(fdrtool)
-load("/run/user/1000/gvfs/smb-share:server=cyfiles.iastate.edu,share=09/22/ntyet/R/RA/Data/RFI-newdata/result3/Model102.LineDiet/Model102_result.RData")
-str(result$P.values[[3]])
-hist(result$P.values[[3]][,"LineDiet"], nclass = 100)
 
-?grenander
-
-z <- result$P.values[[3]][,"LineDiet"]
-e <- ecdf(z)
-g <- grenander(e)
-g
-plot(g, log = "y")
-plot(g)
-plot(density(z))
-sum( g$f.knots[-length(g$f.knots)]*diff(g$x.knots) )
-install.packages('ADGofTest')
-library('ADGofTest')
-?ad.test
-ad.test(g$F.knots, punif)
-plot(g$x.knots, g$F.knots)
-sqrt(sum((g$F.knots - g$x.knots)^2))
-
-?ks.test
-g$f.knots
-ks.test(z, punif)
-
-
+# model 1
 load("/run/user/1000/gvfs/smb-share:server=cyfiles.iastate.edu,share=09/22/ntyet/R/RA/Data/RFI-newdata/result4/Model1.Line.Diet.RFI.Concb.RINb.Conca.RINa.lneut.llymp.lmono.leosi.lbaso.Block.Blockorder/Model1_result.RData")
-str(result$P.values[[3]])
-ks.test(result$P.values[[3]][,"Blockorder"], punif)
+# Crames Von Miser distances
+cvm <- apply(result$P.values[[3]][,colnames(result$P.values[[3]])], 2, function(z)
+  {
+  e <- ecdf(z)
+  g <- grenander(e)
+  cvm <- sum((g$F.knots - g$x.knots)^2 *diff(c(0,g$x.knots)))
+  cvm
+})
+
+which.min(cvm)
+
+
+# Proportion of pvalue less than 0.05
+pvalue_05 <- apply(result$P.values[[3]][,colnames(result$P.values[[3]])]<=0.05, 2, sum)
+which.min(pvalue_05)
+order(pvalue_05)
+order(cvm)
+
+# model 2
+load("/run/user/1000/gvfs/smb-share:server=cyfiles.iastate.edu,share=09/22/ntyet/R/RA/Data/RFI-newdata/result4/Model2.Line.Diet.RFI.Concb.RINb.Conca.RINa.lneut.llymp.lmono.leosi.lbaso.Block/Model2_result.RData")
+# Crames Von Miser distances
+cvm <- apply(result$P.values[[3]][,colnames(result$P.values[[3]])], 2, function(z)
+{
+  e <- ecdf(z)
+  g <- grenander(e)
+  cvm <- sum((g$F.knots - g$x.knots)^2 *diff(c(0,g$x.knots)))
+  cvm
+})
+
+which.min(cvm)
+
+
+# Proportion of pvalue less than 0.05
+pvalue_05 <- apply(result$P.values[[3]][,colnames(result$P.values[[3]])]<=0.05, 2, sum)
+which.min(pvalue_05)
+order(pvalue_05)
+order(cvm)
+
+
+# model 3
+load("/run/user/1000/gvfs/smb-share:server=cyfiles.iastate.edu,share=09/22/ntyet/R/RA/Data/RFI-newdata/result4/Model3.Line.Diet.RFI.Concb.RINb.Conca.RINa.lneut.llymp.lmono.lbaso.Block/Model3_result.RData")
+# Crames Von Miser distances
+cvm <- apply(result$P.values[[3]][,colnames(result$P.values[[3]])], 2, function(z)
+{
+  e <- ecdf(z)
+  g <- grenander(e)
+  cvm <- sum((g$F.knots - g$x.knots)^2 *diff(c(0,g$x.knots)))
+  cvm
+})
+
+which.min(cvm)
+
+
+# Proportion of pvalue less than 0.05
+pvalue_05 <- apply(result$P.values[[3]][,colnames(result$P.values[[3]])]<=0.05, 2, sum)
+which.min(pvalue_05)
+order(pvalue_05)
+order(cvm)
+
+
+# model 4
+load("/run/user/1000/gvfs/smb-share:server=cyfiles.iastate.edu,share=09/22/ntyet/R/RA/Data/RFI-newdata/result4/Model4.Line.RFI.Concb.RINb.Conca.RINa.lneut.llymp.lmono.lbaso.Block/Model4_result.RData")
+# Crames Von Miser distances
+cvm <- apply(result$P.values[[3]][,colnames(result$P.values[[3]])], 2, function(z)
+{
+  e <- ecdf(z)
+  g <- grenander(e)
+  cvm <- sum((g$F.knots - g$x.knots)^2 *diff(c(0,g$x.knots)))
+  cvm
+})
+
+which.min(cvm)
+
+
+# Proportion of pvalue less than 0.05
+pvalue_05 <- apply(result$P.values[[3]][,colnames(result$P.values[[3]])]<=0.05, 2, sum)
+which.min(pvalue_05)
+order(pvalue_05)
+order(cvm)
+
+
+
+# model 5
+load("/run/user/1000/gvfs/smb-share:server=cyfiles.iastate.edu,share=09/22/ntyet/R/RA/Data/RFI-newdata/result4/Model5.Line.RFI.Concb.RINb.RINa.lneut.llymp.lmono.lbaso.Block/Model5_result.RData")
+# Crames Von Miser distances
+cvm <- apply(result$P.values[[3]][,colnames(result$P.values[[3]])], 2, function(z)
+{
+  e <- ecdf(z)
+  g <- grenander(e)
+  cvm <- sum((g$F.knots - g$x.knots)^2 *diff(c(0,g$x.knots)))
+  cvm
+})
+
+which.min(cvm)
+
+
+# Proportion of pvalue less than 0.05
+pvalue_05 <- apply(result$P.values[[3]][,colnames(result$P.values[[3]])]<=0.05, 2, sum)
+which.min(pvalue_05)
+order(pvalue_05)
+order(cvm)
+
+# model 6
+load("/run/user/1000/gvfs/smb-share:server=cyfiles.iastate.edu,share=09/22/ntyet/R/RA/Data/RFI-newdata/result4/Model6.Line.Concb.RINb.RINa.lneut.llymp.lmono.lbaso.Block/Model6_result.RData")
+# Crames Von Miser distances
+cvm <- apply(result$P.values[[3]][,colnames(result$P.values[[3]])], 2, function(z)
+{
+  e <- ecdf(z)
+  g <- grenander(e)
+  cvm <- sum((g$F.knots - g$x.knots)^2 *diff(c(0,g$x.knots)))
+  cvm
+})
+
+which.min(cvm)
+
+
+# Proportion of pvalue less than 0.05
+pvalue_05 <- apply(result$P.values[[3]][,colnames(result$P.values[[3]])]<=0.05, 2, sum)
+which.min(pvalue_05)
+order(pvalue_05)
+order(cvm)
+
+
+# model 7
+load("/run/user/1000/gvfs/smb-share:server=cyfiles.iastate.edu,share=09/22/ntyet/R/RA/Data/RFI-newdata/result4/Model7.Line.Concb.RINa.lneut.llymp.lmono.lbaso.Block/Model7_result.RData")
+# Crames Von Miser distances
+cvm <- apply(result$P.values[[3]][,colnames(result$P.values[[3]])], 2, function(z)
+{
+  e <- ecdf(z)
+  g <- grenander(e)
+  cvm <- sum((g$F.knots - g$x.knots)^2 *diff(c(0,g$x.knots)))
+  cvm
+})
+
+which.min(cvm)
+
+
+# Proportion of pvalue less than 0.05
+pvalue_05 <- apply(result$P.values[[3]][,colnames(result$P.values[[3]])]<=0.05, 2, sum)
+which.min(pvalue_05)
+order(pvalue_05)
+order(cvm)
+
+
+# model 8
+load("/run/user/1000/gvfs/smb-share:server=cyfiles.iastate.edu,share=09/22/ntyet/R/RA/Data/RFI-newdata/result4/Model8.Line.Concb.RINa.lneut.llymp.lmono.Block/Model8_result.RData")
+# Crames Von Miser distances
+cvm <- apply(result$P.values[[3]][,colnames(result$P.values[[3]])], 2, function(z)
+{
+  e <- ecdf(z)
+  g <- grenander(e)
+  cvm <- sum((g$F.knots - g$x.knots)^2 *diff(c(0,g$x.knots)))
+  cvm
+})
+
+which.min(cvm)
+
+
+# Proportion of pvalue less than 0.05
+pvalue_05 <- apply(result$P.values[[3]][,colnames(result$P.values[[3]])]<=0.05, 2, sum)
+which.min(pvalue_05)
+order(pvalue_05)
+order(cvm)
+
