fd06999181058bd98f61e43fafabaad1ec543f0f
nguyenty <tienyettoan@gmail.com>
2014-07-19 12:39:06 -0500

    code

--- simulation.R
+++ simulation.R
@@ -12,130 +12,8 @@ counts <- as.matrix(scount[rowSums(scount[,-1]>0)>3&
 
 covset <- read.table("covset.txt")
 attach(covset)
-table(covset$Line, covset$Block)
-## put histograms on the diagonal
-panel.hist <- function(x, ...)
-{
-  usr <- par("usr"); on.exit(par(usr))
-  par(usr = c(usr[1:2], 0, 1.5) )
-  h <- hist(x, plot = FALSE)
-  breaks <- h$breaks; nB <- length(breaks)
-  y <- h$counts; y <- y/max(y)
-  rect(breaks[-nB], 0, breaks[-1], y, col = "cyan", ...)
-}
-pairs(USJudgeRatings[1:5], panel = panel.smooth,
-      cex = 1.5, pch = 24, bg = "light blue",
-      diag.panel = panel.hist, cex.labels = 2, font.labels = 2)
-
-## put (absolute) correlations on the upper panels,
-## with size proportional to the correlations.
-panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor, ...)
-{
-  usr <- par("usr"); on.exit(par(usr))
-  par(usr = c(0, 1, 0, 1))
-  r <- abs(cor(x, y))
-  txt <- format(c(r, 0.123456789), digits = digits)[1]
-  txt <- paste0(prefix, txt)
-  if(missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
-  text(0.5, 0.5, txt, cex = cex.cor * r)
-}
-pairs(USJudgeRatings, lower.panel = panel.smooth, upper.panel = panel.cor)
-
-pairs(log(result$P.values[[3]][]), diag.panel = panel.hist,
-      upper.panel = panel.cor)
-
-pairs(fit$coef[], diag.panel = panel.hist,
-      upper.panel = panel.cor)
 
 
-covset <- read.table("covset.txt")
-str(covset)
-str(fit$coef)
-colnames(fit$coef)
-pairs(fit$coef)
-str(fit$LRT)
-colnames(fit$LRT)
-
-str(result$P.values[[3]])
-pairs(result$P.values[[3]])
-# intercept
-hist(fit$coef[,1], nclass = 100)
-
-# Line2
-
-hist(fit$coef[,2], nclass = 100)
-shapiro.test(fit$coef[,2])
-
-# Concb
-hist(fit$coef[,3], nclass = 100)
-
-# RINa
-hist(fit$coef[,4], nclass = 100)
-
-# lneut
-hist(fit$coef[,5], nclass = 100)
-# llymp
-hist(fit$coef[,6], nclass = 100)
-
-# lmono
-hist(fit$coef[,7], nclass = 100)
-
-# lbaso
-hist(fit$coef[,8], nclass = 100)
-
-# Block2 3 4
-hist(fit$coef[,9], nclass = 100)
-hist(fit$coef[,10], nclass = 100)
-hist(fit$coef[,11], nclass = 100)
-
-
-load("Model5_result.RData")
-str(result)
-load("Model5_fit.RData")
-str(fit)
-str(fit$coef)
-colnames(fit$coef)
-
-str(fit$LRT)
-colnames(fit$LRT)
-# intercept
-hist(fit$coef[,1], nclass = 100)
-# Line2
-
-hist(fit$coef[,2], nclass = 100)
-shapiro.test(fit$coef[,2])
-
-# Concb
-hist(fit$coef[,3], nclass = 100)
-
-# RFI
-hist(fit$coef[,4], nclass = 100)
-
-# lneut
-hist(fit$coef[,5], nclass = 100)
-# llymp
-hist(fit$coef[,6], nclass = 100)
-
-# lmono
-hist(fit$coef[,7], nclass = 100)
-
-# lbaso
-hist(fit$coef[,8], nclass = 100)
-
-# Block2 3 4
-hist(fit$coef[,9], nclass = 100)
-hist(fit$coef[,10], nclass = 100)
-hist(fit$coef[,11], nclass = 100)
-hist(fit$coef[,12], nclass = 100)
-hist(fit$coef[,13], nclass = 100)
-hist(fit$coef[,14], nclass = 100)
-hist(fit$coef[,15], nclass = 100)
-hist(fit$coef[,16], nclass = 100)
-hist(fit$coef[,17], nclass = 100)
-covset <- read.table("covset.txt")
-str(covset)
-str(fit$NB.disp)
-hist(fit$NB.disp, nclass = 50)
 # check if the condition of count avarage is sastified
 # change to simulation setup
 #estimate the number 
@@ -170,68 +48,46 @@ pvalue_line <- result$P.values[[3]][,"Line"]
 qvalue_line <- result$Q.values[[3]][,"Line"]
 gre_out <- pval.hist.grenander(pvalue_line)
 ebp_line <- gre_out$h.ebp
-hist(ebp_line, nclass = 100)
-hist(pvalue_line, nclass = 100)
-length(pvalue_line)
-result$m0["QLSpline", "Line"]
-sum(ebp_line >0.5)/length(pvalue_line)
-plot(ebp_line, pvalue_line)
-
-
-hist(pvalue_line[ebp_line<0.5], nclass = 100)
-hist(fit$coef[,2][ebp_line<0.5], nclass = 100)
-hist(fit$coef[,2], nclass = 100)
-summary(fit$coef[,2])
-boxplot(fit$coef[,2])
-sum(fit$coef[,2][ebp_line<0.5] >0.06)
-sum(fit$coef[,2] >0.06)
-coef_line<- fit$coef[,2]
-plot(ebp_line, coef_line)
-
 fdr_line <- gre_out$h.fdr
-sum(fdr_line <=.10)
-which ()
-plot(pvalue_line, fdr_line)
-plot(pvalue_line, qvalue_line)
-plot(fdr_line, qvalue_line, type = "l")
-s <- sample(length(pvalue_line), 10000)
-ind <- s[order(s)]
-spvalue_line <- pvalue_line[s]
-sebp_line <- pval.hist.grenander(spvalue_line)$h.ebp
-sum(sebp_line>0.5)/length(spvalue_line)
-full_model <- model.matrix(~Line + Concb + RINa + lneut + llymp + lmono + lbaso + Block)
 Block <- as.factor(Block)
 Line <- as.factor(Line)
+
+full_model <- model.matrix(~Line + Concb + RINa + lneut + llymp + lmono + lbaso + Block)
+dim(full_model)
 coef_beta <- fit$coef 
 coef_beta[,2] <- fit$coef[,2]*(ebp_line<0.5)
-dim(coef_beta)
-colnames(full_model)
 set.seed(1)
 J <- 1000
 s <- sample(dim(coef_beta)[1], J)
 s <- s[order(s)]
-mu <- coef_beta%*%t(full_model)
+
+offset <- apply(counts, 2, quantile, 0.75)
+lf=length(offset)
+
+R=matrix(rep(offset,dim(coef_beta)[1]),
+         ncol=lf,
+         byrow=T)
+
+
+mu <- exp(coef_beta%*%t(full_model))* R
 omega <- fit$NB.disp
-  degene <- which((ebp_line<0.5))
+degene <- which((ebp_line<0.5))
+
 s_mu <- mu[s,]
 s_omega <- omega[s]
-
-degene <- which(ebp_line<0.5)
-
-s_degene <- s[s%in%degene]  
+s_degene <- intersect(degene, s) 
 
 
 ##Sim counts data
-  y <- array(0, dim = c(J,31))
-  
-  for(j in 1:J){
-    repeat{
-        for(k in 1:31){
-          y[j,k]  <- rnbinom(n=1, size=1/s_omega[j], mu=s_mu[j,k])
-        }
-      }
-      if (mean(y[j,])>1& sum(y[j,]>0)>3) break
-    }
-  }
+y <- array(0, dim = c(J,31))
 
-y
+for(j in 1:J){
+  repeat{
+      for(k in 1:31){
+        y[j,k]  <- rnbinom(n=1, size=1/s_omega[j], mu=s_mu[j,k])
+      }
+      if (mean(y[j,])>8& sum(y[j,]>0)>3) break
+    }
+}
+
+
