97722222ba56bf45acd41e95c85ef1ad556e1161
unknown <ntyet@ts3-stat.stat.iastate.edu>
2014-11-03 11:55:46 -0600

    multiple log offset

---
+++ MultipleLogOffSet.R
@@ -0,0 +1,127 @@
+require(Matrix)
+#library(QuasiSeq)
+library(edgeR)
+require(reshape)
+require(plyr)
+library(fields)
+library(reshape)
+library(fdrtool)
+source("QL.fit.R")
+source("NBDev.R")
+source("PoisDev.R")
+source("QL.results.R")
+scount <- read.table("paired end uniquely mapped reads count table.txt", 
+                     header = T)
+row.names(scount) <- scount[,1]
+scount <- scount[-c(which(scount[,1] %in%"ENSSSCG00000007978"),
+                    which(scount[,1] %in%"ENSSSCG00000014725")),]
+
+
+cbc <- read.table('CBC data for pigs with RNA-seq data avaible.txt',
+                  header =T)
+
+metadata <- read.table("meta_data_RNA-seq_G9P2.txt", 
+                       header = T)
+
+rfiadj <- read.csv("g8p2_g9p2-rfiadj-FINAL_Jan_2014_rfiadjusted.csv", 
+                   header = T)
+
+##### cleaning data####
+
+
+
+cbc <- cbc[order(cbc$ear), ]
+metadata <- metadata[order(metadata$idpig), ]
+rfiadj <- rfiadj[order(rfiadj$idpig),]
+
+fullidpig <- as.numeric(paste("20900", metadata$idpig, sep = ""))
+
+covset <- cbind(metadata[, -4], rfiadj[rfiadj$idpig %in% fullidpig, c("rfi.ADJUSTED")], 
+                cbc[, c("iddam", "idsire", "Neutrophil", "Lymphocyte", "Monocyte",
+                        "Eosinophil", "Basophil" )])
+colnames(covset) <- c("idpig", "Line", "Diet",  "Block", "Blockorder", "Concb", 
+                      "RINb", "Conca", "RINa", "RFI",
+                      "iddam", "idsire", "neut",   
+                      "lymp", "mono","eosi", "baso")
+
+#####set of covariates considered ####
+covset <- cbind(covset, lneut = log(covset$neut), llymp = log(covset$lymp), 
+                lmono = log(covset$mono), leosi = log(covset$eosi), 
+                lbaso = log(covset$baso)) 
+covset$Line <- as.factor(covset$Line)
+covset$Diet <- as.factor(covset$Diet)
+covset$Block <- as.factor(covset$Block)
+covset$Blockorder <- as.factor(covset$Blockorder)
+covset$iddam <- as.factor(covset$iddam)
+covset$idsire <- as.factor(covset$idsire)
+levels(covset$idsire) <- 1:11
+levels(covset$iddam) <- 1:20
+
+covset[, c("iddam", "idsire")]
+#detach(covset)
+attach(covset)
+
+# counts <- as.matrix(scount[rowSums(scount[,-1]>0)>3&
+#                              rowMeans(scount[,-1])>8 & 
+#                              rowSums(scount[,-1][,Line ==1] > 0) >0 &
+#                              rowSums(scount[,-1][, Line ==2] >0) >0 ,-1])
+
+counts <- as.matrix(scount[rowSums(scount[,-1]>0)>3&
+                             rowMeans(scount[,-1])>8 ,-1])
+
+load("U:/R/RA/Data/RFI-newdata/resultpairedlogcbc/pvalue05/Model1.Line.Diet.RFI.Concb.RINb.Conca.RINa.lneut.llymp.lmono.leosi.lbaso.Block.Blockorder/Model1_fit.RData")
+load("U:/R/RA/Data/RFI-newdata/resultpairedlogcbc/pvalue05/Model1.Line.Diet.RFI.Concb.RINb.Conca.RINa.lneut.llymp.lmono.leosi.lbaso.Block.Blockorder/Model1_result.RData")
+
+dim(fit$coef)
+#dl2: design MATRIX; b: fit coefficients, w: fit NB.disp
+f <- function(m) class(try(solve(m),silent=T))=="matrix"
+se.b1 <- function(b,w,dl2){
+  
+  X <- dl2
+  
+  eta <- c(X%*%b)
+  
+  mu <- exp(eta)
+  
+  fish <- t(X)%*%(diag(nrow(X))*mu/(mu*w+1))%*%X
+  
+  if(f(fish)){
+    fish.inv <- solve(fish)
+    return(sqrt(diag(fish.inv)))
+  }else{
+    return(rep(0, ncol(X)))
+  }
+}
+
+
+prefit <- fit
+model_th <- 1
+full_model <- model.matrix(~Line + Diet + RFI + Concb + RINb + Conca + RINa + 
+                             lneut + llymp + lmono + leosi + lbaso + 
+                             Block + Blockorder)
+
+#prefit is the output of QL.fit using the full model including covariates
+# 
+# b <- prefit$coefficients[j,]
+# w <- prefit$NB.disp[j]
+# dl2 <- full_model
+
+ses <- matrix(0, nrow = nrow(prefit$coefficients), ncol = ncol(prefit$coefficients))
+
+for(j in 1:nrow(prefit$coefficients)){ 
+  se.new <- se.b1(prefit$coefficients[j,],prefit$NB.disp[j],full_model)
+  ses[j,] <- se.new
+}
+dim(ses)
+
+which(apply(ses, 1, sum)==0)
+
+
+j <- 2647 # 7299, 9257, 11574
+j <- 1
+se.b1(prefit$coefficients[j,],prefit$NB.disp[j],full_model)
+ load("new.beta.RData")
+str(new.beta[[1]])
+dim(full_model)
+colnames(full_model)
+full_model[, -c(1, 2)]
